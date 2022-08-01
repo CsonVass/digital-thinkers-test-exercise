d3feb5b6177624896227e339690540f7623a1bfe
vasscsongor <vasscsongor@sch.bme.hu>
2022-08-01 16:20:35 +0200

    Random places

--- src/backend/Formula.Backend/DAL/Repository/FormulaRepository.cs
+++ src/backend/Formula.Backend/DAL/Repository/FormulaRepository.cs
@@ -51,12 +51,34 @@ namespace DAL.Repository
             {
                 string json = r.ReadToEnd();
                 List<Driver> drivers = JsonConvert.DeserializeObject<List<Driver>>(json);
+
+                List<int> places = Enumerable.Range(1, drivers.Count).ToList();
+
+                places = Shuffle(places);
+                int i = 0;
                 foreach (Driver d in drivers)
                 {
+                    d.Place = places[i++];
                     ctx.Drivers.Add(d);
                 }
                 ctx.SaveChanges();
             }
+        }
+
+        //helper method for shuffling the places
+        private static Random rng = new Random();
+        public static List<int> Shuffle(List<int> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                int value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+            return list;
         }
     }
 
